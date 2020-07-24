5df9c64ee51732af5be8e82f0f6104fe079f9abc
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-07-24 17:32:12 +0300

    feat: navigation effects and refactoring

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -3,28 +3,30 @@ import styles from './index.module.css';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
+    
 
     return (
         <div className={styles['menu-list']}>
             <ul>
-                <li>
-                    <Link to="/mouse">MOUSE</Link>
+                <li className={styles['special-effects']} >
+                    <Link  to="/">MOUSE</Link>
                 </li>
-                <li>
-                    <Link to="/keyboard">KEYBOARDS</Link>
+                <li className={styles['special-effects']}>
+                    <Link to="/">KEYBOARDS</Link>
                 </li>
-                <li>
-                    <Link to="/headset">HEADSETS</Link>
+                <li className={styles['special-effects']}>
+                    <Link to="/">HEADSETS</Link>
                 </li>
-                <li>
-                    <Link to="/mousepad">MOUSEPADS</Link>
+                <li className={styles['special-effects']}>
+                    <Link to="/">MOUSEPADS</Link>
                 </li>
-                <li>
-                    <Link to="/accessoaries">ACCESSORIES</Link>
+                <li className={styles['special-effects']}>
+                    <Link to="/">ACCESSORIES</Link>
                 </li>
             </ul>
         </div>
     );
 }
+
 
 export default Navigation
